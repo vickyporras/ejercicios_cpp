fc0f96550bbdba618821a417cc56018e11868158
vickyporras <porrasortizvictoria@gmail.com>
2022-03-21 18:34:29 +0100

    intento de hacer, fallido

--- ejercicio_tablero.cpp
+++ ejercicio_tablero.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
 
+void imprimirTablero(string palo){
+  for()
+}
 
 int main(){
-
+// np entiendo como lo voy a imprimir si el bucle for es con int y yo voy a utilizar string
 
 
   return 0;
