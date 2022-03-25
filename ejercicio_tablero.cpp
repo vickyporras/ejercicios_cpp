83e4a973caf62412eb78ca8b7d610dbe670682db
vickyporras <porrasortizvictoria@gmail.com>
2022-03-25 10:27:56 +0100

    creamos clase tablero

--- ejercicio_tablero.cpp
+++ ejercicio_tablero.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void imprimirTablero(string palo){
-  for()
-}
+class Tablero{
+private:
+  int filas;
+  int columnas;
+  int **tablero;
+public:
+  Tablero(){
+    this->filas=5;
+    this->columna=5;
+  }
+};
 
 int main(){
-// np entiendo como lo voy a imprimir si el bucle for es con int y yo voy a utilizar string
 
 
   return 0;
